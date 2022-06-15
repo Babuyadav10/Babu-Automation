2159f538efa19d35978bb6340e2cde2fd7dea2b3
sayyed-imran <91603426+sayyed-imran@users.noreply.github.com>
2022-06-15 15:37:49 +0530

    save

--- src/test/java/CT_APITesting/BasicTest.java
+++ src/test/java/CT_APITesting/BasicTest.java
@@ -3,32 +3,40 @@ package CT_APITesting;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
+import io.restassured.RestAssured;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class BasicTest extends BaseTest {
 
-        String Email,AccessToken;
-        ArrayList<String> emailValues = new ArrayList<String>();
-        int EmailCounts;
+        public HashMap<String,Object> userJson;
 
+//        String Email,AccessToken;
+//        ArrayList<String> emailValues = new ArrayList<String>();
+//        int EmailCounts;
+        String createdAPIKey;
 
         @Test(priority = 0)
         public void userDetails(){
+
+
+                RestAssured.given().
+                        pathParam("appId","")
 
                 String responseDetails = given().queryParam("page","2").header("Content-Type","application/json")
                         .when().get("api/users")
                         .then().assertThat().statusCode(200).body("page",equalTo(2)).extract().body().asString();
 
                 PathFinder(responseDetails);
-                Email = js.getString("data.email[0]");
-                EmailCounts = js.getInt("per_page");
-
-                for (int i=0;i<EmailCounts;i++){
-                        Email = js.getString("data.email["+i+"]");
-                        emailValues.add(Email);
-                }
+//                Email = js.getString("data.email[0]");
+//                EmailCounts = js.getInt("per_page");
+//
+//                for (int i=0;i<EmailCounts;i++){
+//                        Email = js.getString("data.email["+i+"]");
+//                        emailValues.add(Email);
+//                }
 
 
 
@@ -39,19 +47,19 @@ public class BasicTest extends BaseTest {
 
         @Test(priority = 1)
         public void login(){
-                for(int j=0;j<emailValues.size();j++) {
-                        String responseLogin = given().body("{\n" +
-                                "    \"email\": \"" + emailValues.get(j) + "\",\n" +
-                                "    \"password\": \"cityslicka\"\n" +
-                                "}").header("Content-Type", "application/json")
-                                .when().post("api/login")
-                                .then().assertThat().statusCode(200)
-                                .extract().asString();
-
-                        PathFinder(responseLogin);
-                        AccessToken = js.getString("token");
-                        System.out.println("Access Token:- "+AccessToken+" Email ID:- "+emailValues.get(j));
-                }
+//                for(int j=0;j<emailValues.size();j++) {
+//                        String responseLogin = given().body("{\n" +
+//                                "    \"email\": \"" + emailValues.get(j) + "\",\n" +
+//                                "    \"password\": \"cityslicka\"\n" +
+//                                "}").header("Content-Type", "application/json")
+//                                .when().post("api/login")
+//                                .then().assertThat().statusCode(200)
+//                                .extract().asString();
+//
+//                        PathFinder(responseLogin);
+//                        AccessToken = js.getString("token");
+//                        System.out.println("Access Token:- "+AccessToken+" Email ID:- "+emailValues.get(j));
+//                }
 
                 //System.out.println("ResponseBody:- "+responseLogin);
 
