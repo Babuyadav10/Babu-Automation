f200b2202f9135cbffa13a715b92b6e9c8dd21b1
sayyed-imran <91603426+sayyed-imran@users.noreply.github.com>
2022-06-22 18:45:04 +0530

    users test done

--- src/test/java/CT_APITesting/UsersTest.java
+++ src/test/java/CT_APITesting/UsersTest.java
@@ -80,5 +80,94 @@ public class UsersTest extends BaseTest {
 
     }
 
+    @Test(priority = 3)
+    public void reactivateUsers(){
+
+
+        String responseBody=given().
+                header("apiKey",mainApiKey).
+                header("Accept","application/json").
+                header("Content-Type","application/json").
+                body("{\"uidsToActivate\": [\""+createdUserId+"\"]}").
+                when().
+                put("/users").
+                then().assertThat().statusCode(200).extract().body().asString();
+
+        System.out.println(responseBody);
+
+        PathFinder(responseBody);
+        String str =js.getString("data.reactivatedUids");
+
+        Assert.assertEquals(str.substring(1,str.length() - 1),createdUserId);
+
+
+    }
+
+    @Test(priority = 4)
+    public void getRoles(){
+        String responseBody=given().
+                header("apiKey",mainApiKey).
+                header("onBehalfOf",createdUserId).
+                header("Accept","application/json").
+                get("/users/"+createdUserId).then().
+                assertThat().statusCode(200).extract().body().asString();
+
+        System.out.println(responseBody);
+
+        PathFinder(responseBody);
+
+        Assert.assertEquals(createdUserId,js.getString("data.uid"));
+
+
+    }
+
+    @Test(priority = 5)
+    public void updateRoles(){
+
+        /*String updatedRoleName= getRandomString("newRoleName");
+
+
+
+        JSONObject userJson = new JSONObject();
+        userJson.put("name",updatedRoleName);
+        userJson.put("description","test descriptin of update role"+updatedRoleName);
+
+
+        String responseBody=given().
+                header("apiKey",mainApiKey).
+                header("Accept","application/json").
+                header("Content-Type","application/json").
+                body(userJson.toString(1)).
+                when().
+                put("/roles/"+createdRole).then().
+                assertThat().statusCode(200).extract().body().asString();
+
+        System.out.println(responseBody);
+
+        PathFinder(responseBody);
+
+        Assert.assertEquals(updatedRoleName,js.getString("data.name"));
+*/
+
+
+
+    }
+
+    @Test(priority = 6)
+    public void deleteRoles()
+
+    {
+        given().
+                header("apiKey",mainApiKey).
+                header("Accept","application/json").
+                header("Content-Type","application/json").
+                when().
+                delete("/users/"+createdUserId).
+                then().assertThat().statusCode(200);
+
+
+
+    }
+
 
 }
