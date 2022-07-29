db03eeecc8e0062730c9c90a0931c846ef7cfc33
babu-yadav <97106214+babu-yadav@users.noreply.github.com>
2022-07-29 12:21:53 +0530

    GroupTest

--- src/test/java/CT_APITesting/GroupTest.java
+++ src/test/java/CT_APITesting/GroupTest.java
@@ -1,6 +1,7 @@
 package CT_APITesting;
 
 import org.json.JSONObject;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -14,7 +15,7 @@ public class GroupTest extends BaseTest {
 //        int EmailCounts;
     String createdGuid;
     String createdGroupName;
-    String createdGroupType="public";
+    String createdGroupType = "public";
     String mainApiKey = "10ca9c4268ffa7ef032de02e8606da7e3bf67b4f";
 
 
@@ -23,12 +24,12 @@ public class GroupTest extends BaseTest {
 
         String guid = getRandomString("guid");
         String groupName = getRandomString("groupName");
-       // String groupType = getRandomString("groupType");
+        // String groupType = getRandomString("groupType");
 
         JSONObject userJson = new JSONObject();
         userJson.put("guid", guid);
         userJson.put("name", groupName);
-        userJson.put("type",createdGroupType);
+        userJson.put("type", createdGroupType);
         userJson.put("description", "anim amet voluptate non");
         String responseBody = given().
                 header("apiKey", mainApiKey).header("Content-Type", "application/json").header("Accept", "application/json").
@@ -44,6 +45,85 @@ public class GroupTest extends BaseTest {
         createdGroupName = js.getString("data.name");
         createdGroupType = js.getString("data.type");
         System.out.println(createdGuid);
+
+    }
+
+
+    @Test(priority = 1, description = "Verify list Groups")
+    public void listGroups() {
+
+        String responseBody = given().
+                header("apiKey", mainApiKey).
+                header("Accept", "application/json").
+                header("Content-Type", "application/json").
+                when().
+                get("/groups").
+                then().
+                assertThat().statusCode(200).extract().body().asString();
+
+        System.out.println(responseBody);
+
+        PathFinder(responseBody);
+        Assert.assertEquals(js.getString("data[0].guid"), createdGuid);
+    }
+
+    @Test(priority = 2, description = "verify get groups")
+    public void getGroups() {
+        String responseBody = given().
+                header("apiKey", mainApiKey).
+                header("Accept", "application/json").
+                header("Content-Type", "application/json").
+                get("/groups/" + createdGuid).then().
+                assertThat().statusCode(200).extract().body().asString();
+
+        System.out.println(responseBody);
+
+        PathFinder(responseBody);
+
+        Assert.assertEquals(createdGuid, js.getString("data.guid"));
+
+
+    }
+
+    @Test(priority = 3, description = "Verify update Group functionality")
+    public void updateGroups() {
+
+        String updatedGroupName = getRandomString("newGroupName");
+
+
+        JSONObject userJson = new JSONObject();
+        userJson.put("name", updatedGroupName);
+        userJson.put("description", "test description of update group" + updatedGroupName);
+
+
+        String responseBody = given().
+                header("apiKey", mainApiKey).
+                header("Accept", "application/json").
+                header("Content-Type", "application/json").
+                body(userJson.toString(1)).
+                when().
+                put("/groups/" + createdGuid).then().
+                assertThat().statusCode(200).extract().body().asString();
+
+        System.out.println(responseBody);
+
+        PathFinder(responseBody);
+
+        Assert.assertEquals(updatedGroupName, js.getString("data.name"));
+
+
+    }
+
+
+    @Test(priority = 4, description = "Verify delete Groups functionality")
+    public void deleteGroups() {
+        given().
+                header("apiKey", mainApiKey).
+                header("Accept", "application/json").
+                when().
+                delete("/groups/" + createdGuid).
+                then().assertThat().statusCode(200);
+
 
     }
 }
